edf3f20a4023b02633a49eea5668332bcfab6913
jfreiche <jul.freiche@gmail.com>
2017-09-29 13:46:54 +0200

    modified version

--- TP JEE/TPv2/tpv2/tpv2-ejb/src/main/java/fr/cpe/jms/producer/impl/HelloJmsProducer.java
+++ TP JEE/TPv2/tpv2/tpv2-ejb/src/main/java/fr/cpe/jms/producer/impl/HelloJmsProducer.java
@@ -1,13 +1,18 @@
 package fr.cpe.jms.producer.impl;
 
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 import javax.inject.Inject;
 import javax.jms.JMSContext;
 import javax.jms.Queue;
 
 import fr.cpe.jms.producer.IHelloJmsProducer;
 
+@Startup
+@Singleton
 public class HelloJmsProducer implements IHelloJmsProducer{
 	
 	@Resource(name="java:/asi2-hello")
@@ -20,6 +25,11 @@ public class HelloJmsProducer implements IHelloJmsProducer{
 	public void sayHello(String name) {
 		context.createProducer()
 		.send(queue,name);
+	}
+	
+	@PostConstruct
+	private void exec(){
+		sayHello("world !");
 	}
 
 }
